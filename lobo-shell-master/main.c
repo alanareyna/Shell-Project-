92f3b957a7c003be20c1518f6e90e10448075396
Andrew Avola <76986167+Meatballin@users.noreply.github.com>
2022-10-26 13:51:12 -0700

    SegFault when using strcpy()
    Logic seems okay so far in my head, just need to get around the segfault or figure something better out.

--- lobo-shell-master/main.c
+++ lobo-shell-master/main.c
@@ -47,25 +47,36 @@ For more than 1 pipe:
 
 */
 
+//Functions prototypes
 int checkForPipes(char** line_words);
+char*** storeAllCommands(char** line_words, int pipe_counter);
+void storeInto(char** dest, char** src, int indexToStartAt);
 int main()
 {
     // Buffer for reading one line of input
     char line[MAX_LINE_CHARS];
     // holds separated words based on whitespace
     char* line_words[MAX_LINE_WORDS + 1];
+    char*** beegYoshi;
 
     int pipeCounter = 0;
 
     // or some other input error occurs
-    while( fgets(line, MAX_LINE_CHARS, stdin) ) {
-
+    while( fgets(line, MAX_LINE_CHARS, stdin) ) 
+    {
         //line is char Array
         //line_words is array of strings
         int num_words = split_cmd_line(line, line_words);
+
         //Number of pipes in our entire command line
         pipeCounter = checkForPipes(line_words);
-        switch(pipeCounter){
+
+        //num_strings is number of strings in entire command minus the number of pipes in our entire cmd line
+        int num_strings = num_words - pipeCounter;
+
+        //Switch passed # of pipes and logic will be taken care of for each test
+        switch(pipeCounter)
+        {
             case 0:
                 pid_t pid;
                 if(pid = fork() == 0)
@@ -73,8 +84,16 @@ int main()
                     execvp(line_words[0], line_words);
                     while(wait(NULL) != -1);
                 }
-            
-    }
+            case 1:
+                // pid_t pid1;
+                // pid_t pid2;
+                // int pfd1[2];
+                // int pfd2[2];
+                beegYoshi = storeAllCommands(line_words, pipeCounter);
+                // printf("%s, ", beegYoshi[0]);
+
+
+        }
 
     }
 
@@ -93,4 +112,55 @@ int checkForPipes(char** line_words)
             returnPipes++;
     }
     return returnPipes;
+}
+
+// [1] = addr of string array [2] = string itself [3] = char inside of string
+char*** storeAllCommands(char** line_words, int pipe_counter)
+{
+    //Allocate memory for the number of arrays of strings we are pointing to
+    //For example, if we have one pipe in the entire command line, we will have two full commands(pipeCounter + 1)
+
+    //New strategy: Dynamically allocate new string arrays and have returningArray point to them.
+    //After execd in main, free (all of the indexes to returningArray? or just the base address? not sure which needs to be done)
+    char*** returningArray = malloc(sizeof(char***) * (pipe_counter + 1));
+    int returnIndex = 0;
+    int countWords = 0;
+    int startCounter = 0;
+    int idxToStart = -1;
+    for(int i = 0; line_words[i] != NULL; i++)
+    {
+        
+        if((strcmp(line_words[i], "|") == 0))
+        {
+            //We are going to store the whole string up until a pipe in this newly dynamically
+            //allocated ptr to strings. Then after this, point returningArray at a specified index to the
+            //base address of this newly allocated array.
+            idxToStart += 1;
+            char** tempToBeAssigned = malloc(sizeof(char**) * countWords);
+            storeInto(tempToBeAssigned, line_words, idxToStart);
+            exit(1);
+            returningArray[returnIndex] = tempToBeAssigned;
+            returnIndex++;
+            idxToStart = startCounter + 1;
+            countWords = 0;
+            
+        }
+        countWords++;
+        startCounter++;
+    }
+    return returningArray;
+    
+}
+
+void storeInto(char** dest, char** src, int indexToStartAt)
+{   
+    
+    
+    
+    //SEG FAULT RIGHT HERE THERE BE A SEG FAULT RIGHT HERE AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+    // for(int i = 0, j = indexToStartAt; (strcmp(src[j], "|") != 0) || (src[j] != NULL); i++, j++)
+    // {
+    //     strcpy(dest[i], src[j]);
+    // }
+    
 }
