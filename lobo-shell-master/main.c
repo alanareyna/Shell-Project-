277fee870595dce365fd1bc04e18dec27afe2bff
Andrew Avola <76986167+Meatballin@users.noreply.github.com>
2022-11-01 22:07:43 -0700

    Test 1, 2, 3, 4 WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- lobo-shell-master/main.c
+++ lobo-shell-master/main.c
@@ -9,61 +9,30 @@
 #include "constants.h"
 #include "parsetools.h"
 #include <stdbool.h>
-/*Andrew Notes:
 
-NOTE: It seems that the total number of fork() calls depends on our number of
-pipes(|) + 1. So we will need an incrementation of a pipe counter in terms of how
-many times it shows up in our line_words.
-
-Note: We might need to look into dynamic memory allocation for allocating enough memory for pidt array
-to be passed into pipe() function. This will be dependant on our actual pipe (|) counter that we will be updating 
-as we parse through our linwe_words.
-
-Note: Might need to separate line_words into separate array of strings (char**) that begin at 
-[0] index so that we can pass the execvp(assuming we have to use execvp in the first place)
-
-For 1 command Tests:
-    - We keep track of the number of strings that are going to be 
-      stored from our split_cd_line() function. If we find no pipe (|)
-      characters, i.e our pipe counter is equal to zero, that implies
-      that we should only have one command in our array. This should fork() once.
-      We can probably use execlp for one command.
-
-For 1 pipe:
-    - We can parse through the function and keep a counter of how many pipe (|) 
-      characters we have in line_words which is a single character array. If we only
-      have one pipe, that means we should have two valid commands on each side of
-      that singular pipe. Write end of first Fork() FD should go to write end of singular
-      pipe, Read end should go to stdin(default), and err should go to screen(default). Write end
-      of second Fork() goes to stdout(default), read end of FD at read end of singular pipe
-      and ERR fd remains in default screen. (This is all variable to change based on our actual commands
-      and what they can possibly be).
-
-For more than 1 pipe:
-     - Number of forks is going to be pipe(|) countter + 1. Logic of first and last Fork() FD's is  
-       similar to our logic in "1 command tests", everything in between will be similar to the logic
-       in "For 1 pipe". 
-       
-
-*/
-struct{
-    int isBeginning;
-}beginning = {1};
+// struct{
+//     int isBeginning;
+// }beginning = {1};
 
 //Functions prototypes
 int checkForPipes(char** line_words);
 char*** storeAllCommands(char** line_words, int pipe_counter);
 void storeInto(char** dest, char** src, int indexToStartAt, int countWords);
+void closePfd(int pfd[][2], int pipeCounter);
+char **createTempArray(char **line_words, int wordCount, int *indexesToStart, int index);
+int *whichIdxToStart(char **line_words, int pipeCounter);
+int countWordsBetweenPipes(char **line_words, int *indexesToStart, int index);
 int main()
 {
     // Buffer for reading one line of input
     char line[MAX_LINE_CHARS];
     // holds separated words based on whitespace
     char* line_words[MAX_LINE_WORDS + 1];
-    char*** beegYoshi;
+    // char*** commandsArray;
     pid_t pid;
     int pipeCounter = 0;
-
+    char** arrForChild;
+    int wordCount;
     // or some other input error occurs
     while( fgets(line, MAX_LINE_CHARS, stdin) ) 
     {
@@ -76,154 +45,153 @@ int main()
         
         //num_strings is number of strings in entire command minus the number of pipes in our entire cmd line
         int num_strings = num_words - pipeCounter;
-
         
-        //Switch passed # of pipes and logic will be taken care of for each test
-        switch(pipeCounter)
+        
+        //Check for no pipes
+        if(pipeCounter == 0)
         {
-            
-            case 0:
+            if(pid = fork() == 0)
+            {
+                execvp(line_words[0], line_words);
+                return 0;
+                
+            }
+        }
+        else if(pipeCounter > 0)
+        {
+            int pfdN[pipeCounter][2];
+            for(int j = 0; j < pipeCounter; j++)
+                pipe(pfdN[j]);
+
+            int *indexesToStart = whichIdxToStart(line_words, pipeCounter);
+        
+            for(int i = 0; i < pipeCounter + 1; i++)
+            {
+
+                wordCount = countWordsBetweenPipes(line_words, indexesToStart, i);
+                // printf("%d, ", wordCount);
+                // exit(1);
+                arrForChild = createTempArray(line_words, wordCount, indexesToStart, i);
+                // for(int x = 0; arrForChild[x] != NULL; x++)
+                //     printf("%s, ", arrForChild[x]);
+                // printf("\n");
+                // free(arrForChild);
+                // //fork child process
                 if(pid = fork() == 0)
                 {
-                    execvp(line_words[0], line_words);
-                    while(wait(NULL) != -1);
-                    
-                }
-                break;
-            case 1:
-                //printf("In case 1, pipecounter = %d: ", pipeCounter);
-                int pfd[2];
-                
-                beegYoshi = storeAllCommands(line_words, pipeCounter);
-                pipe(pfd);
-
-                for(int i = 0; i < (pipeCounter + 1); i++)
-                {
-                    
-                    //Parent needs to control when at beginning and end process
-                    beginning.isBeginning = (i == 0) ? 1 : 0;
-                    pid = fork();
-                    if(pid == -1)
+                    if(i == 0)
                     {
-                        printf("Error creating child process\n");
-                        exit(1);
+                        //printf("At beginning\n");
+                        dup2(pfdN[i][1], 1);
+                        
                     }
-                    if(pid == 0)
+                    else if(i == pipeCounter)
                     {
-                        //Child fork execution
-                        switch(beginning.isBeginning)
-                        {
-                            //Case 0 is end process logic
-                            case 0:
-                                dup2(pfd[0], 0);
-                                if(close(pfd[0]) == -1 || close(pfd[1]) == -1)
-                                    printf("Error closing pfd2 from end process\n");
-                                execvp(beegYoshi[1][0], beegYoshi[1]);
-                                return 0;
-                            //Case 1 is beginning process logic
-                            case 1:
-                                dup2(pfd[1], 1);
-                                if(close(pfd[1]) == -1 || close(pfd[0]) == -1)
-                                    printf("Error closing pfd2 from beginning process\n");
-                                execvp(beegYoshi[0][0], beegYoshi[0]);
-                                return 0;
-                            default: 
-                                break;
-                        };
+                        //printf("At end\n");
+                        dup2(pfdN[i - 1][0], 0);
+                        
                     }
-                    
+                    else if((i != 0) && i < pipeCounter)
+                    {
+                        //printf("%d, ", i);
+                        //printf("At middle command\n");
+                        dup2(pfdN[i - 1][0], 0);
+                        dup2(pfdN[i][1], 1);
+                        
+                    }
+                    closePfd(pfdN, pipeCounter);
+                    execvp(arrForChild[0], arrForChild);
+                    return 0;
+                   
                 }
-                
-                
-                //reap children and close fds
-                if(close(pfd[0]) == -1 || close(pfd[1]) == -1)
-                {
-                    printf("Parent had error closing stdin and stdout");
-                    exit(1);
-                }
-                
-                while(wait(NULL) != -1){
-                };
-                break;
+            }
+            closePfd(pfdN, pipeCounter);
+            while(wait(NULL) != -1){
+            };
             
-            default:
-                
-                break;
-        };
-
-    }
-
+        }
+       
+    };
     return 0;
+
 }
 
+    
 
 
+
+
+int countWordsBetweenPipes(char **line_words, int *indexesToStart, int index)
+{
+    int wordCount = 0;
+    
+    for(int i = indexesToStart[index]; line_words[i] != NULL ; i++)
+    {
+        if(line_words[i] == NULL || strcmp(line_words[i], "|") == 0)
+            break;
+        wordCount++;
+    } 
+
+    return wordCount;
+}
+
+int *whichIdxToStart(char **line_words, int pipeCounter)
+{
+    int j = 1;
+    int *tempArray = malloc(sizeof(int) * pipeCounter + 1);
+    tempArray[0] = 0;
+    for(int i = 0; line_words[i] != NULL; i++)
+    {
+        if(strcmp(line_words[i], "|") == 0)
+        {
+            tempArray[j] = i + 1;
+            j++;
+        }
+    }
+    return tempArray;
+}
+char **createTempArray(char **line_words, int wordCount, int *indexesToStart, int index)
+{
+    int i = 0;
+    int j = indexesToStart[index];
+
+    char **temp = malloc(sizeof(char**) * wordCount + 1);
+    for(int words = 0; words < wordCount; words++)
+        temp[words] = malloc(sizeof(char*) *  1024);
+    
+    for(; i < wordCount; i++, j++)
+        // printf("%s, ", line_words[j]);
+        strcpy(temp[i], line_words[j]);
+    
+    //Null terminate the strings or crash
+    temp[i] = 0x0;
+    
+    return temp;
+        
+}
+
+void closePfd(int pfd[][2], int pipeCounter)
+{
+    
+    for(int i = 0; i < pipeCounter; i++)
+    {
+        if(close(pfd[i][0]) == -1 || close(pfd[i][1]) == -1)
+        {
+            printf("Error closing pfd2 from beginning process\n");
+            exit(1);
+        }
+            
+    }
+}
 //Counting pipes function
 int checkForPipes(char** line_words)
 {
     int returnPipes = 0;
     for(int i = 0; line_words[i] != NULL; i++)
     {
-        if((strcmp(line_words[i], "|") == 0))
+        if(strcmp(line_words[i], "|") == 0)
             returnPipes++;
     }
     return returnPipes;
 }
 
-// [1] = addr of string array [2] = string itself [3] = char inside of string
-char*** storeAllCommands(char** line_words, int pipe_counter)
-{
-    //Allocate memory for the number of arrays of strings we are pointing to
-    //For example, if we have one pipe in the entire command line, we will have two full commands(pipeCounter + 1)
-
-    //New strategy: Dynamically allocate new string arrays and have returningArray point to them.
-    //After execd in main, free (all of the indexes to returningArray? or just the base address? not sure which needs to be done)
-    char*** returningArray = malloc(sizeof(char***) * (pipe_counter + 1));
-    int returnIndex = 0;
-    int countWords = 0;
-    int startCounter = 0;
-    bool start = true;
-    int idxToStart = -1;
-    for(int i = 0; line_words[i] != NULL; i++)
-    {
-        //need to check for no pipes and store again
-        if((strcmp(line_words[i], "|") == 0) || line_words[i + 1] == NULL)
-        {
-            //We are going to store the whole string up until a pipe in this newly dynamically
-            //allocated ptr to strings. Then after this, point returningArray at a specified index to the
-            //base address of this newly allocated array.
-            if(start)
-            {
-                idxToStart = 0;
-                start = false;
-            }  
-            
-            char** tempToBeAssigned = malloc(sizeof(char**) * countWords);
-            for(int i = 0; i < countWords; i++)
-            {
-                tempToBeAssigned[i] = malloc(sizeof(char*) * 1024);
-            }
-            storeInto(tempToBeAssigned, line_words, idxToStart, countWords);
-            idxToStart = i + 1;
-            returningArray[returnIndex] = tempToBeAssigned;
-            returnIndex++;
-            countWords = 0;
-
-            
-        }
-        countWords++;
-       
-    }
-    return returningArray;
-    
-}
-
-void storeInto(char** dest, char** src, int indexToStartAt, int countWords)
-{   
-
-    for(int i = 0, j = indexToStartAt; i < countWords ; i++, j++)
-    {
-        strcpy(dest[i], src[j]);
-    }
-    
-}
